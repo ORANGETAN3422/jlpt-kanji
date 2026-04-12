37f3e6a6c6568dc466edaa5e52f3050fbf352e13
ORANGETAN3422 <orangtan3422@gmail.com>
2026-04-12 20:49:54 +1000

    jamboree hate train

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,7 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
-	kit: { adapter: adapter() },
-	paths: {base: "/jlpt-kanji/"}
+	kit: { adapter: adapter(), paths: {base: "/jlpt-kanji/"} },
 };
 
 export default config;
